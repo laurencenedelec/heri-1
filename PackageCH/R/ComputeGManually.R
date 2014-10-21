214138ce1a52dcb25f3321d526d5ac0545c689df
Duvanel Julien <julien@duvanel.ch>
2014-10-21 16:21:37 -0700

    Update ComputeGManually
    Former-commit-id: 7f591b5e21222af6c5fc996653a8a76f6b59a84a

--- PackageCH/R/ComputeGManually.R
+++ PackageCH/R/ComputeGManually.R
@@ -2,19 +2,22 @@ library(doMC)
 library(foreach)
 registerDoMC(16)
 
-N <- 20
+setwd("/home/duvanel/git/PackageCH/")
 
-# Get results (stored into SNPS_datetime.stamp.raw)
-Phenotypes <- read.table("data/SNPs_14102014_145630.raw", header = TRUE)
-# Remove rows with NA's 
-Phenotypes <- Phenotypes[complete.cases(Phenotypes), ]
-# We must have "famid" and "id" as the two first columns
-colnames(Phenotypes) <- c("famid", "id")
-
-# Value of SNPs are stored in the last N columns
-snps <- Phenotypes[, (ncol(Phenotypes)-N+1):ncol(Phenotypes)]
-
-build_matrix_G <- function(snps) {
+build_matrix_G <- function(path.snps = "data/SNPs_14102014_145630.raw") {
+    
+    N <- 20
+    
+    # Get results (stored into SNPS_datetime.stamp.raw)
+    Phenotypes <- read.table(path.snps, header = TRUE)
+    # Remove rows with NA's 
+    Phenotypes <- Phenotypes[complete.cases(Phenotypes), ]
+    # We must have "famid" and "id" as the two first columns
+    colnames(Phenotypes) <- c("famid", "id")
+    
+    # Value of SNPs are stored in the last N columns
+    snps <- Phenotypes[, (ncol(Phenotypes)-N+1):ncol(Phenotypes)]
+    
     G <- matrix(0, 
                 ncol = nrow(snps),
                 nrow = nrow(snps))
@@ -30,6 +33,33 @@ build_matrix_G <- function(snps) {
     G
 } 
 
-#G <- build_matrix_G(snps)
-#save(list = "G", file = "G.RData")
-
+HeritabilityEstimation <- function() {
+    
+    G <- build_matrix_G(snps)
+    load("data/Phenotype_Simulated_14102014_145630.RData")
+    
+    P <- as.matrix(P.raw$fake.trait)
+    
+    ### We keep one phenotype and normalize its
+    V <- matrix(P, 
+                nrow = nrow(P),
+                ncol = ncol(P))
+    
+    V <- t(t(V) - colMeans(V))
+    V.sd <- colSds(V)
+    
+    # Return
+    V <- t(t(V) * (1/V.sd))
+    
+    # Get data for the specific phenotype   
+    Y.distance.carre <- matrix(rep(V^2,nrow(V)),
+                               ncol = nrow(V)) + 
+        t(matrix(rep(V^2,nrow(V)),
+                 ncol = nrow(V))) - 
+        2 * V %*%t(V)
+    
+    Y.distance <- sqrt(Y.distance.carre)
+    
+    heritability <- dcov(Y.distance, G) / (sqrt(dcov(G, G)) * sqrt(dcov(Y.distance, Y.distance)))
+    
+}
