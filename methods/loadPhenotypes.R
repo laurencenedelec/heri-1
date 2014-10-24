ef6676c115b2c2be7a6c3cf6f9662429ba855ca1
Julien Duvanel <julien@duvanel.ch>
2014-10-24 15:05:07 -0700

    Update

--- methods/loadPhenotypes.R
+++ methods/loadPhenotypes.R
@@ -5,7 +5,8 @@ library(testthat)
 P.raw <- read.csv(file = "data/PhenoData.csv")
 #load("data/Phenotype_Simulated.RData")
 #load("data/Phenotype_Simulated_14102014_104636.RData")
-load("data/Phenotype_Simulated_14102014_145630.RData")
+#load("data/Phenotype_Simulated_14102014_145630.RData")
+load("data/Phenotype_Simulated_24102014_104043.RData")
 
 P.raw$full_id <- paste(P.raw$famid, P.raw$id)
 rownames(P.raw) <- P.raw$full_id
