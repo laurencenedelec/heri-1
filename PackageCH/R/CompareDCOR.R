4c9e11a4849623e7a3fdd2da0763aac4e6877427
Duvanel Julien <julien@duvanel.ch>
2014-10-30 17:00:46 -0700

    Update, setting a few configurations for Main_3.R (DCOR comparisons)

--- PackageCH/R/CompareDCOR.R
+++ PackageCH/R/CompareDCOR.R
@@ -1,9 +1,26 @@
 library(energy)
 library(matrixStats)
 
+#' Build alpha s.t. X = G * alpha
+#'
+#' @title Build a random SNps matrix
+#' @param N_real_coeff number of non_zero coeff
+#' @param N_SNPs number of SNPS
+#' @param b value of runif(b[1],b[2])
+#' @return alpha
+#' @author Julien Duvanel
+#' @export
+build_alpha <- function(N_real_coeff, N_SNPS, b) {
+    
+    # build alpha
+    alpha <- sample(c(runif(N_real_coeff, b[1], b[2]), 
+                      rep(x = 0, times = N_SNPS - N_real_coeff)))
+    
+}
+
 #' Build a random SNps matrix
 #'
-#' @title GBuild a random SNps matrix
+#' @title Build a random SNps matrix
 #' @param N sample size
 #' @param N_SNPs number of SNPS
 #' @param snps_value value that SNPs can take
@@ -35,6 +52,7 @@ compare_dcor <- function(N,
                          N_SNPS, 
                          N_real_coeff, 
                          get_snps_matrix,
+                         get_alpha,
                          b,
                          variable = "",
                          AddNoise = FALSE) {
@@ -49,8 +67,10 @@ compare_dcor <- function(N,
         
         # Build a fake genome matrix
         M <- get_snps_matrix(N[i], N_SNPS[i])
-        alpha <- sample(c(runif(N_real_coeff[i], b[1], b[2]), rep(x = 0, times = N_SNPS[i] - N_real_coeff[i])))
-        
+        alpha <- get_alpha(N_real_coeff = N_real_coeff[i], 
+                           N_SNPS = N_SNPS[i], 
+                           b = b)
+                
         # Build fake trait X
         X <- M %*% alpha
         
@@ -60,34 +80,42 @@ compare_dcor <- function(N,
         
         # Do dcor estimation
         res <- rbind(res, 
-                     c(get(variable[1])[i], dcor(X, M)))
+                     c(get(variable[1])[i], 
+                       dcor(X, M),
+                       # This last line is used to compare dcor(X,Y)
+                       # when X and Y are completly independent
+                       # (this is done by simulating X and then generating a new SNPs matrix)
+                       dcor(X, get_snps_matrix(N[i], N_SNPS[i]))))
         
         cat("-> i = ", i, "/", length(N), "\n")
     }
     
+    res <- data.frame(var = res[,1],
+                      X_from_G = res[,2],
+                      X_not_from_G = res[,3])
+
     # Export a pdf file
-    pdf(file = paste0("results/plots/ExperimentalDcor_", format(Sys.time(), "%d%m%Y_%H%M%S") ,".pdf"), width = 17, height = 7)
-    
-        txtNoise <- ""
-        if(AddNoise == TRUE) txtNoise <- " + N(0,1)"
-        
-        plot(res, 
-             xlab = paste0("Value of ", paste(variable, collapse=", ")), 
-             ylab = "dcor(X,G)",
-             main = paste0("X = G * vec of runif(", b[1], ",", b[2],  ")", txtNoise, ", N in ", min(N), ":", max(N),
+    txtNoise <- ""
+    if(AddNoise == TRUE) txtNoise <- " + N(0,1)"
+
+    p <- ggplot(data = melt(res, measure.vars = c("X_from_G", "X_not_from_G")),
+                aes_string(x = "var" , y = "value")) +
+            geom_point(aes_string(color = "variable")) +
+            xlab(paste0("Value of ", paste(variable, collapse=", "))) +
+            ylab("dcor(X,G)") + 
+            ggtitle(paste0("X = G * vec of runif(", b[1], ",", b[2],  ")", txtNoise, ", N in ", min(N), ":", max(N),
                            ", N_SNPS in ", min(N_SNPS), ":", max(N_SNPS),
                            " and N_real_coeff in ", min(N_real_coeff), ":", max(N_real_coeff)))
+
+    ggsave(plot = p,
+           filename = paste0("results/plots/ExperimentalDcor_", 
+                             format(Sys.time(), "%d%m%Y_%H%M%S"),
+                             ".pdf"),
+           width = 17, height = 7)
     
-    dev.off()
-    
-    list( res = res)
-    
+    # return
+    list(res = res, X = X, M = M)    
 }
-
-
-
-
-
 
 # X.cov <- cov(X)
 # M.cov <- cov(M)
