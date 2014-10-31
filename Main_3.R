8c157701dfc84ca786931fcb0675c268a8649a05
Duvanel Julien <julien@duvanel.ch>
2014-10-30 21:48:27 -0700

    Update

--- Main_3.R
+++ Main_3.R
@@ -36,46 +36,47 @@ Project <- SetupProject()
 
 param.list <- list()
 
-N_Estimation <- 20
-param.list[[1]] <- list(N = rep(400, times = N_Estimation),
-                        N_SNPS = seq(from = 10, to = 1000, by = 1000/N_Estimation),
+N_Estimation <- 500
+N_MAX <- 5000
+param.list[[1]] <- list(N = rep(500, times = N_Estimation),
+                        N_SNPS = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
                         N_real_coeff = rep(0, times = N_Estimation),
                         b = c(2,2),
                         variable = "N_SNPS")                   
 
-param.list[[2]] <- list(N = rep(400, times = N_Estimation),
-                        N_SNPS = seq(from = 10, to = 1000, by = 1000/N_Estimation),
+param.list[[2]] <- list(N = rep(500, times = N_Estimation),
+                        N_SNPS = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
                         N_real_coeff = rep(1, times = N_Estimation),
                         b = c(2,2),
                         variable = "N_SNPS")
 
-param.list[[3]] <- list(N = rep(400, times = N_Estimation),
-                        N_SNPS = seq(from = 10, to = 1000, by = 1000/N_Estimation),
+param.list[[3]] <- list(N = rep(500, times = N_Estimation),
+                        N_SNPS = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
                         N_real_coeff = rep(10, times = N_Estimation),
                         b = c(2,2),
                         variable = "N_SNPS")
 
 param.list[[4]] <- list(N = rep(20, times = N_Estimation),
-                        N_SNPS = seq(from = 20, to = 1000, by = 1000/N_Estimation),
-                        N_real_coeff = rep(20, times = N_Estimation),
+                        N_SNPS = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
+                        N_real_coeff = rep(10, times = N_Estimation),
                         b = c(2,2),
                         variable = "N_SNPS")
 
-param.list[[5]] <- list(N = rep(1000, times = N_Estimation),
-                        N_SNPS = rep(1000, times = N_Estimation),
-                        N_real_coeff = seq(from = 20, to = 1000, by = 1000/N_Estimation),
+param.list[[5]] <- list(N = rep(N_MAX, times = N_Estimation),
+                        N_SNPS = rep(N_MAX, times = N_Estimation),
+                        N_real_coeff = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
                         b = c(2,2),
                         variable = "N_real_coeff")
 
-param.list[[6]] <- list(N = seq(from = 20, to = 1000, by = 1000/N_Estimation),
+param.list[[6]] <- list(N = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
                         N_SNPS = rep(20, times = N_Estimation),
-                        N_real_coeff = rep(20, times = N_Estimation),
+                        N_real_coeff = rep(10, times = N_Estimation),
                         b = c(2,2),
-                        variable = "N_real_coeff")
+                        variable = "N")
 
-param.list[[7]] <- list(N = seq(from = 20, to = 1000, by = 1000/N_Estimation),
-                        N_SNPS = seq(from = 20, to = 1000, by = 1000/N_Estimation),
-                        N_real_coeff = rep(20, times = N_Estimation),
+param.list[[7]] <- list(N = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
+                        N_SNPS = seq(from = 10, to = N_MAX, by = N_MAX/N_Estimation),
+                        N_real_coeff = rep(10, times = N_Estimation),
                         b = c(2,2),
                         variable = c("N", "N_SNPS"))
 
@@ -89,7 +90,8 @@ for(i in 1:length(param.list)) {
                         N_SNPS = param.list[[i]]$N_SNPS,
                         N_real_coeff = param.list[[i]]$N_real_coeff, 
                         get_snps_matrix = build_SNPs_matrix, 
-                        get_alpha = build_alpha,
+                        get_alpha = build_alpha_dominant,
+                        product_snps_alpha = product_snps_alpha_dominant,
                         b = param.list[[i]]$b, 
                         variable = param.list[[i]]$variable)
         
--- PackageCH/R/CompareDCOR.R
+++ PackageCH/R/CompareDCOR.R
@@ -4,7 +4,7 @@ library(reshape2)
 
 #' Build alpha s.t. X = G * alpha
 #'
-#' @title Build a random SNps matrix
+#' @title Build alpha
 #' @param N_real_coeff number of non_zero coeff
 #' @param N_SNPs number of SNPS
 #' @param b value of runif(b[1],b[2])
@@ -17,6 +17,55 @@ build_alpha <- function(N_real_coeff, N_SNPS, b) {
     alpha <- sample(c(runif(N_real_coeff, b[1], b[2]), 
                       rep(x = 0, times = N_SNPS - N_real_coeff)))
     
+}
+
+#' Build alpha s.t. X = G * alpha but alpha(g_i)
+#'
+#' @title Build alpha
+#' @param N_real_coeff number of non_zero coeff
+#' @param N_SNPs number of SNPS
+#' @param b value of runif(b[1],b[2])
+#' @return alpha with dominant effect
+#' @author Julien Duvanel
+#' @export
+build_alpha_dominant <- function(N_real_coeff, N_SNPS, b) {
+    
+    # We create an alpha for the dominant effect
+    alpha_d <- matrix(0, nrow = 3, ncol = N_SNPS)
+    alpha_d[1, ] <- sample(c(runif(N_real_coeff, -1, 1), 
+                             rep(x = 0, times = N_SNPS - N_real_coeff)))
+    alpha_d[2, ] <- sample(c(runif(N_real_coeff, 3, 4), 
+                             rep(x = 0, times = N_SNPS - N_real_coeff)))
+    alpha_d[3, ] <- sample(c(runif(N_real_coeff, 15, 20), 
+                             rep(x = 0, times = N_SNPS - N_real_coeff)))
+    
+    alpha_d
+
+}
+
+#' mat * alpha but alpha(mat)
+#'
+#' @title Product between mat and alpha for dominant effect
+#' @param M a matrix with N_SNPS columns
+#' @param alpha a matrix with 3 rows and N_SNPS columsn
+#' @return product of mat * alpha
+#' @author Julien Duvanel
+#' @export
+product_snps_alpha_dominant <- function(M, alpha) {
+    # Usually, as.matrix(mat) %*% alpha does the job
+    # but here, the multiplication differs if mat[i,j] has 
+    # a specific value.
+    
+    # For every row, we do the "special product"
+    res <- apply(M, 1, function(x) {
+        ret <- 0
+        for(i in 1:length(x)) {
+            ret <- ret + x[i]*alpha[x[i]+1, i]
+        }
+        ret
+    })
+    
+    as.matrix(res)
 }
 
 #' Build a random SNps matrix
@@ -54,7 +103,8 @@ compare_dcor <- function(N,
                          get_snps_matrix,
                          get_alpha,
                          b,
-                         variable = "") {
+                         variable = "",
+                         product_snps_alpha = function(M, alpha) { M %*% alpha }) {
     
     # We have to check that dimensions agree
     if(length(N) != length(N_SNPS) | length(N) != length(N_real_coeff)) stop("Problem, length of N has to be the same as N_SNPS.")    
@@ -71,7 +121,7 @@ compare_dcor <- function(N,
                            b = b)
                 
         # Build fake trait X
-        X <- M %*% alpha
+        X <- product_snps_alpha(M, alpha)
 
         # Do dcor estimation
         res <- rbind(res, 
@@ -97,6 +147,7 @@ compare_dcor <- function(N,
             geom_point(aes_string(color = "variable"), size = 2, position = position_jitter(w = 1.5, h = 0)) +
             xlab(paste0("Value of ", paste(variable, collapse=", "))) +
             ylab("dcor(X,G)") + 
+            scale_y_continuous(limits = c(0, 1)) +
             ggtitle(paste0("X = G * vec of runif(", b[1], ",", b[2],  "), N in ", min(N), ":", max(N),
                            ", N_SNPS in ", min(N_SNPS), ":", max(N_SNPS),
                            " and N_real_coeff in ", min(N_real_coeff), ":", max(N_real_coeff)))
