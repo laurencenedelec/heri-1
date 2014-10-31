cba243160e32f2887f9d82c70fe83b178f4d79c8
Duvanel Julien <julien@duvanel.ch>
2014-10-30 17:00:46 -0700

    Update, setting a few configurations for Main_3.R (DCOR comparisons)
    Former-commit-id: 4c9e11a4849623e7a3fdd2da0763aac4e6877427

--- Main_3.R
+++ Main_3.R
@@ -36,7 +36,7 @@ Project <- SetupProject()
 
 param.list <- list()
 
-N_Estimation <- 50
+N_Estimation <- 100
 param.list[[1]] <- list(N = rep(400, times = N_Estimation),
                         N_SNPS = seq(from = 10, to = 1000, by = (1000-10+10)/N_Estimation),
                         N_real_coeff = rep(0, times = N_Estimation),
@@ -65,13 +65,30 @@ param.list[[4]] <- list(N = rep(400, times = N_Estimation),
                         variable = "N_SNPS",
                         AddNoise = FALSE)
 
+param.list[[1]] <- list(N = rep(400, times = N_Estimation),
+                        N_SNPS = seq(from = 10, to = 1000, by = 1000/N_Estimation),
+                        N_real_coeff = rep(10, times = N_Estimation),
+                        b = c(2,2),
+                        variable = "N_SNPS",
+                        AddNoise = TRUE)
+
+
+
+res <- list()
 # Do estimation and generate parameters
 for(i in 1:length(param.list)) {
-    res <- compare_dcor(N = param.list[[i]]$N, 
+    
+    cat(paste0("= Param list: ", i, "/", length(param.list), "\n"))
+    
+    res[[i]] <- compare_dcor(N = param.list[[i]]$N, 
                         N_SNPS = param.list[[i]]$N_SNPS,
                         N_real_coeff = param.list[[i]]$N_real_coeff, 
                         get_snps_matrix = build_SNPs_matrix, 
+                        get_alpha = build_alpha,
                         b = param.list[[i]]$b, 
                         variable = param.list[[i]]$variable,
                         AddNoise = param.list[[i]]$AddNoise)
+        
+    cat(paste0("= End of ", i, "/", length(param.list), "\n"))
+                       
 }
