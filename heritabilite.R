d1e6243a158ee45455ca45326977c860d7f80cac
Laurence Nedelec <nedelec@math.stanford.edu>
2014-12-14 20:21:36 -0800

    18 dec

--- heritabilite.R
+++ heritabilite.R
@@ -15,25 +15,10 @@ pheno<-testpheno[,c(-3,-4,-5,-6,-7,-8)]
 pheno.order<-join(tfam[,c(1,2)],pheno[,c(2,3,4,5,6)],"SUBJID")   
 rownames(pheno.order) <- paste(tfam$SUBJID)
 ##for those pheno add the individu id 3=K 4=H 5=L 6=T
-Kpheno<-data.matrix(pheno.order[,c(1,2,3)])
 
-#write.table(Kpheno,file="~/NFG/raw/Kpheno",row.names=F,col.names=F)
-Hpheno<-data.matrix(pheno.order[,c(1,2,4)])                                        
-# write.table(Hpheno,file="~/NFG/raw/Hpheno",row.names=F,col.names=F)
-Lpheno<-data.matrix(pheno.order[,c(1,2,5)])                                        
-# write.table(Lpheno,file="~/NFG/raw/Lpheno",row.names=F,col.names=F)
-Tpheno<-data.matrix(pheno.order[,c(1,2,6)])
-# write.table(Tpheno,file="~/NFG/raw/Tpheno",row.names=F,col.names=F)    
-
-# Hpheno<- read.table("~/NFG/raw/Hpheno")
-# Lpheno<-read.table("~/NFG/raw/Lpheno")
-# Tpheno<-read.table("~/NFG/raw/Tpheno")
-# Hpheno<-data.matrix(Hpheno)
-# Lpheno<-data.matrix(Lpheno)
-# Tpheno<-data.matrix(Tpheno)
 
 ##Keep the pheno you wants normalize pheno
-pheno<-matrix(c(Hpheno[,3],Lpheno[,3],Tpheno[,3],Kpheno[,3]),ncol=4)
+pheno<-data.matrix( pheno.order[,c(4,5,6,3)])
 rownames(pheno) <- paste(tfam$SUBJID)
 pheno.t<-t(pheno)-colMeans(pheno)
 pheno<-t(pheno.t)
@@ -80,10 +65,10 @@ for (l in 1:ncol(Kdis)) {ok <- !is.na(Kdis[,l])
 Kdis<-Kdis[as.vector(id),as.vector(id)]
 norm<-dcov_mc(Kdis,Kdis,1)
 Kdis.list<-as.numeric(c(Kdis))
-Kdis.mean<-mean(Kdis.list)
-list<- Kdis.list<(2)*Kdis.mean
+
+
 print(norm)
-print(Kdis.mean)
+#print(Kdis.mean)
 #for all pheno 
 for (i in 1:4) 
 {print(i)
@@ -109,12 +94,18 @@ heri.lm.sans<-lm(Y.produit.list.sans~Kdis.list.sans)
 print(length(Kdis.list.sans))
 heri.sans<-summary(heri.lm.sans)$coef[2,1]
 pv.heri.sans<-summary(heri.lm.sans)$coef[2,4]
+Kdis.mean<-mean(Kdis.list.sans)
+if (j==1)    red<- Kdis.list.sans<(1.2)*Kdis.mean  
+if (j==2)    red<- Kdis.list.sans<(1.2)*(1-Kdis.mean)
+if (j==4)    red<- Kdis.list.sans<(1.2)*(1-Kdis.mean)
+if (j==3)    red<- Kdis.list.sans<(1.2)*(1-Kdis.mean)
+if (j==5)    red<- Kdis.list.sans<(1.5)*Kdis.mean
+if (j==6)    red<- Kdis.list.sans<(1.5)*Kdis.mean
 
-Kdis.list.red<-as.numeric(as.vector(Kdis.list[list]))
-Y.produit.list.red<-as.numeric( as.vector( Y.produit.list[list]))
+Kdis.list.red<-Kdis.list.sans[red]
+Y.produit.list.red<- Y.produit.list.sans[red]
 heri.lm.red<-lm(Y.produit.list.red~Kdis.list.red)
-print(length(Y.produit.list.red))
-print(length(Kdis.list.red))
+
 
 heri.lm.all<-lm(Y.produit.list~Kdis.list)
 heri.red<-summary(heri.lm.red)$coef[2,1]
@@ -133,24 +124,24 @@ heri.dcov<-dcov_mc(Y.produit,Kdis,1)
 heri.dcov<-heri.dcov/norm
 
 #ne marche pas
-norm.dcov.energy<-(dcov(as.dist(Kdis),as.dist(Kdis),1))^2
-heri.dcov.energy<-(dcov(as.dist(Y.produit),as.dist(Kdis),1))^2
-heri.dcov.energy<-heri.dcov.energy/norm.dcov.energy
+#norm.dcov.energy<-(dcov(as.dist(Kdis),as.dist(Kdis),1))^2
+#heri.dcov.energy<-(dcov(as.dist(Y.produit),as.dist(Kdis),1))^2
+#heri.dcov.energy<-heri.dcov.energy/norm.dcov.energy
 
 #heri.dcov.energy<-1
 #norm.dcov<-1
 
 ##the results
-herita<-c(j,i,heri.sans,pv.heri.sans,heri.red,pv.heri.red,heri.all,pv.heri.all,heri.dcov, heri.dcov.energy, Kdis.mean,norm,norm.dcov.energy)
+herita<-c(j,i,heri.sans,pv.heri.sans,heri.red,pv.heri.red,heri.all,pv.heri.all,heri.dcov, Kdis.mean,)
 res<-rbind(res,herita)
 
 
 }
 }
 res<-data.matrix(res)
-colnames(res)<-c('method', 'pheno','sansdiag','pvsansdiag', 'lm.red','pv.lm.red', 'lm','pv.lm','heri.dcov','heri.dcov.energy','meanK', 'dcov(meanK)','dcov(meanK).energy')
-#rownames(res)<-c('K.ibs','K.idb','K.ibs','K.cgta'," ",'H.ibs','H.idb','H.ibs','H.cgta',"h",'L.ibs','L.idb','L.ibs','L.cgta',"L",'T.ibs','T.idb','T.ibs','T.cgta','T')
-write.table(res,file="~/NFG/ler/res4.heri.RData", row.names=TRUE, col.names=TRUE)
+colnames(res)<-c('method', 'pheno','sansdiag','pvsansdiag', 'lm.red','pv.lm.red', 'lm','pv.lm','heri.dcov','meanK')
+rownames(res)<-c('K.ibs','K.idb','K.ibs','K.cgta',"k ","kk",'H.ibs','H.idb','H.ibs','H.cgta',"h",'hh",'L.ibs','L.idb','L.ibs','L.cgta',"L",'ll','T.ibs','T.idb','T.ibs','T.cgta','T','tt')
+write.table(res,file="~/NFG/ler/res5.heri.RData", row.names=TRUE, col.names=TRUE)
 
 
 
